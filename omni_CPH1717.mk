e5860d18a530bc14e8ec28915fc4af4e7e4f93fd
Carlo Dee <carlodee.official@gmail.com>
2022-10-11 13:51:37 +0800

    Revertback to old tree, and added workflow.

--- omni_CPH1717.mk
+++ omni_CPH1717.mk
@@ -7,15 +7,14 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from CPH1717 device
 $(call inherit-product, device/oppo/CPH1717/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := CPH1717
